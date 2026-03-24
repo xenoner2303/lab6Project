095320a72369ff8a60bed493cf35888a934526cd
JoyInDespair <JoyInDespair@KOMPUTER>
2026-03-24 09:17:48 +0200

    add simple tests for calc

--- src/lab6Project/Calculator.cs
+++ src/lab6Project/Calculator.cs
@@ -12,7 +12,7 @@ public class Calculator
         return a - b;
     }
 
-    public int Multiplication(int a, int b)
+    public int Multiply(int a, int b)
     {
         return a * b;
     }
---
+++ tests/lab6Tests/CalculatorTests.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+using MyProject;
+
+namespace lab6Project.tests;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_ShouldReturnCorrectSum()
+    {
+        var calc = new Calculator();
+
+        var result = calc.Add(2, 3);
+
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void Subtract_ShouldReturnCorrectSum()
+    {
+        var calc = new Calculator();
+
+        var result = calc.Subtract(2, 3);
+
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void Multiply_ShouldReturnCorrectSum()
+    {
+        var calc = new Calculator();
+
+        var result = calc.Multiply(2, 3);
+
+        Assert.Equal(6, result);
+    }
+
+    [Fact]
+    public void Divide_ShouldReturnCorrectResult()
+    {
+        var calc = new Calculator();
+
+        var result = calc.Divide(10, 2);
+
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void Divide_ByZeroShouldThrowException()
+    {
+        var calc = new Calculator();
+
+        Assert.Throws<ArgumentException>(() => calc.Divide(10, 0));
+    }
+}
