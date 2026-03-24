b3094856932c7a2de2882d82f28f05c467403f1e
JoyInDespair <JoyInDespair@KOMPUTER>
2026-03-24 09:12:05 +0200

    add base calculator class and methods

---
+++ src/lab6Project/Calculator.cs
@@ -0,0 +1,27 @@
+﻿namespace lab6Project;
+
+public class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Multiplication(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Divide(int a, int b)
+    {
+        if (b == 0)
+            throw new ArgumentException("Ділення на нуль");
+
+        return a / b;
+    }
+}
--- src/lab6Project/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace lab6Project;
-
-public class Class1
-{
-
-}
